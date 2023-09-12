c39ee7e8f3d0f5d08eb2c72c98e01c2bef0d2a00
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2023-09-12 09:33:38 -0300

    Updates :rocket:

--- src/CronicasDoEstudanteSistema.java
+++ src/CronicasDoEstudanteSistema.java
@@ -1,15 +1,19 @@
 public class CronicasDoEstudanteSistema {
     private Cronica[] cronicas;
     private int LIMITECRONICAS;
+    private int contador;
 
     public CronicasDoEstudanteSistema(){
         LIMITECRONICAS = 1000;
+        contador = 0;
         cronicas = new Cronica[LIMITECRONICAS];
     }
 
     public String registraCronica(String nome, String conteudo, String autor){
         try{
             Cronica cronica = new Cronica(nome, conteudo, autor);
+            cronicas[contador] = cronica;
+            contador++;
             return "Crônica cadastrada com sucesso.";
         } catch (Exception e){
             return "Crônica não cadastrada.";
@@ -19,26 +23,30 @@ public class CronicasDoEstudanteSistema {
     public String registraCronica(String nome, String conteudo){
         try{
             Cronica cronica = new Cronica(nome, conteudo);
+            cronicas[contador] = cronica;
+            contador++;
             return "Crônica de autor anônimo cadastrada com sucesso.";
         } catch (Exception e){
             return "Crônica não cadastrada.";
         }
     }
 
-    public int pesquisaCronica(String nome){
-        for (int i = 0; i < cronicas.length; i++) {
-            if(nome.equals(cronicas[i].getNome())){
-                return i;
-            }
-        } return 0;
-    }
-
-    public String leCronica(String nome){
-        int posicao = pesquisaCronica(nome);
-        if(posicao > 0){
-            return cronicas[posicao].toString();
-        } else{
+    public String lerCronica(int posicao){
+        if(cronicas[posicao] != null){
+            cronicas[posicao].incrementaQuantidadeLeituras();
+            return cronicas[posicao].getConteudo();
+        } else {
             return "Crônica não cadastrada.";
         }
+    }
+
+    public String exibirCronicaMaisLida(){
+        Cronica maisLida = cronicas[0];
+        for (int i = 1; i < cronicas.length; i++) {
+            if (cronicas[i].getQuantidadeDeLeituras() > maisLida.getQuantidadeDeLeituras()){
+                maisLida = cronicas[i];
+            }
+        }
+        return maisLida.getConteudo();
     }
 }
