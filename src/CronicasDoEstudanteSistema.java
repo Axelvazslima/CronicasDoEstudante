6f5ca289f8a3baf59523eda3ac7b2eefd89eb2cd
Axelvazslima <axel.vaz.souto.lima@ccc.ufcg.edu.br>
2023-09-12 21:19:51 -0300

    updates :constructiom:

--- src/CronicasDoEstudanteSistema.java
+++ src/CronicasDoEstudanteSistema.java
@@ -4,20 +4,22 @@ public class CronicasDoEstudanteSistema {
     private Chronicle[] chronicles;
     private Booklet[] booklets;
     private int MAXCHRONICLESQUANTITY;
-    private int counter;
+    private int counterChronicles;
+    private int counterBookletChronicles;
 
     public CronicasDoEstudanteSistema(){
         MAXCHRONICLESQUANTITY = 1000;
-        counter = 0;
+        counterChronicles = 0;
+        counterBookletChronicles = 0;
         chronicles = new Chronicle[MAXCHRONICLESQUANTITY];
     }
 
     public String registerChronicle(String nome, String conteudo, LocalDate data, String autor){
         try{
             Chronicle chronicle = new Chronicle(nome, conteudo, data, autor);
-            chronicles[counter] = chronicle;
-            chronicle.setPosition(counter);
-            counter++;
+            chronicles[counterChronicles] = chronicle;
+            chronicle.setPosition(counterChronicles);
+            counterChronicles++;
             return "Crônica cadastrada com sucesso.";
         } catch (Exception e){
             return "Crônica não cadastrada.";
@@ -28,25 +30,31 @@ public class CronicasDoEstudanteSistema {
             Chronicle chronicle = new Chronicle(nome, conteudo, data);
             if (!(checkExistingChronicle(chronicle))){
                 try {
-                    chronicles[counter] = chronicle;
+                    chronicles[counterChronicles] = chronicle;
                 } catch (ArrayIndexOutOfBoundsException aioobe) {
                     return "Crônica não cadastrada. Posição inválida." + aioobe;
                 }
-                chronicle.setPosition(counter);
-                counter++;
+                chronicle.setPosition(counterChronicles);
+                counterChronicles++;
                 return "Crônica de autor anônimo cadastrada com sucesso.";
             }
             return "Crônica já cadastrada. Não é permitido duplicatas.";
     }
 
     public boolean checkExistingChronicle(Chronicle baseChronicle){
-        for (int i = 0; i < counter; i++) {
+        for (int i = 0; i < counterChronicles; i++) {
             if (chronicles[i].equals(baseChronicle)) return true;
         } return false;
     }
 
-    public void creatBooklet(CronicasDoEstudanteSistema system, int[] chronicles){
-        Booklet booklet = new Booklet(system, chronicles);
+    public void createBooklet(CronicasDoEstudanteSistema system, int[] chronicles){
+        try {
+            Booklet booklet = new Booklet(system, chronicles);
+            booklets[counterBookletChronicles] = booklet;
+            counterBookletChronicles++;
+        } catch (Exception e){
+            //
+        }
     }
 
     public String readBooklet(int bookletPosition){
@@ -73,7 +81,7 @@ public class CronicasDoEstudanteSistema {
 
     public String showMostReadChronicle(){
         Chronicle mostRead = chronicles[0];
-        for (int i = 1; i < counter; i++) {
+        for (int i = 1; i < counterChronicles; i++) {
             if (chronicles[i].getReadingQuantity() > mostRead.getReadingQuantity()){
                 mostRead = chronicles[i];
             }
@@ -82,7 +90,7 @@ public class CronicasDoEstudanteSistema {
     }
 
     public void listChronicles(){
-        for (int i = 0; i < counter; i++) {
+        for (int i = 0; i < counterChronicles; i++) {
             System.out.println(chronicles[i]);
         }
     }
